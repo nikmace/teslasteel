bf1b86a73457b62758db43b43e37e3919f860453
Nikita Baranov <nikmacentosh@gmail.com>
2021-07-04 10:53:04 +0300

    Mobile responsive

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -22,7 +22,7 @@ const Contacts = () => {
                 </div>
 
                 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1870.7802550178217!2d23.314163176648886!3d42.66043372648961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa845934c66655%3A0x93edb1d105068b4c!2z0YPQuy4g4oCe0JrQuNGI0LjQvdC10LLigJwgN9CQLCAxNDA3INCl0LvQsNC00LjQu9C90LjQutCwLCDQodC-0YTQuNGP!5e0!3m2!1sru!2sbg!4v1625324818831!5m2!1sru!2sbg" width="800" height="600" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+                <iframe className="google-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1870.7802550178217!2d23.314163176648886!3d42.66043372648961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa845934c66655%3A0x93edb1d105068b4c!2z0YPQuy4g4oCe0JrQuNGI0LjQvdC10LLigJwgN9CQLCAxNDA3INCl0LvQsNC00LjQu9C90LjQutCwLCDQodC-0YTQuNGP!5e0!3m2!1sru!2sbg!4v1625324818831!5m2!1sru!2sbg" width="800" height="600" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
             </div>
         </div>
         
